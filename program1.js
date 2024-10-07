18dd47fc3eae988ca95e930f1f6d7d5f384b3a18
poonam973 <ps2826363@gmail.com>
2024-10-07 15:34:15 +0530

    codes updated

--- program1.js
+++ program1.js
@@ -3,9 +3,35 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    // Dictionary to store the matching pairs of parentheses
+    const matchingBrackets = {
+        ')': '(', 
+        '}': '{', 
+        ']': '['
+    };
+
+    // Stack to store the opening brackets
+    const stack = [];
+
+    // Loop through each character in the string
+    for (let char of s) {
+        // If the character is a closing bracket
+        if (matchingBrackets[char]) {
+            // Pop the top of the stack if it's not empty, otherwise assign a dummy value '#'
+            const topElement = stack.length > 0 ? stack.pop() : '#';
+
+            // Check if the top element matches the corresponding opening bracket
+            if (matchingBrackets[char] !== topElement) {
+                return false;
+            }
+        } else {
+            // If it's an opening bracket, push it onto the stack
+            stack.push(char);
+        }
+    }
+
+    // Return true if the stack is empty (all brackets were matched), otherwise return false
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
--- program2.js
+++ program2.js
@@ -3,8 +3,37 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    // Map to store the Roman numerals and their integer values
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+    let prevValue = 0;
+
+    // Loop through the string from right to left
+    for (let i = s.length - 1; i >= 0; i--) {
+        let currentValue = romanMap[s[i]];
+
+        // If the current value is smaller than the previous one, subtract it
+        if (currentValue < prevValue) {
+            total -= currentValue;
+        } else {
+            // Otherwise, add the current value
+            total += currentValue;
+        }
+
+        // Update the previous value for the next iteration
+        prevValue = currentValue;
+    }
+
+    return total;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
